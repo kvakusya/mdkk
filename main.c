d4fd45b1f189b392a8e18ba2f21753724b35c3d6
Vladislav <vodvud0561@mail.ru>
2023-04-04 11:30:34 +0300

    Ну дела конечно с этими репозиториями...

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
     char name[255];
 
-    printf("Enter your name: ");
+    printf("Ira vse spsisala, postav'te ey 2: ");
     fgets(name, 255, stdin);
     printf("length = %d\n", strlen(name)); /* debug line */
     name[strlen(name)-1] = '\0'; /* remove the newline at the end */
